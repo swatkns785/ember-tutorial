6ae57b5bd20f14ca882837ad5bb47ccc61e0e637
Steven Watkins <steven.watkins78@gmail.com>
2015-04-11 15:16:57 -0400

    created serializers and configured error-defaults

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,6 @@
+mount API::Base, at: "/"
+mount GrapeSwaggerRails::Engine, at: "/documentation"
+
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
